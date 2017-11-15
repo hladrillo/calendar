7603141a69421e52c1ff887ebb780a66b72f015c
herb ladrillo <hladrillo@gmail.com>
2017-11-15 15:07:50 -0500

    final upload

---
+++ events.json.php
@@ -0,0 +1,40 @@
+{
+	"success": 1,
+	"result": [
+		{
+			"id": "293",
+			"title": "Studio Photoshoot",
+			"class": "event-warning",
+			"start": "1510754450000",
+			"end":   "1510761650000"
+		},
+		{
+			"id": "294",
+			"title": "Get Dog Food",
+			"class": "event-success",
+			"start": "1510848050000",
+			"end":   "1510851650000"
+		},
+		{
+			"id": "295",
+			"title": "Get Dinner",
+			"class": "event-success",
+			"start": "1510873250000",
+			"end":   "1510876850000"
+		},
+		{
+			"id": "296",
+			"title": "Finish Garage Painting",
+			"class": "event-warning",
+			"start": "1511186450000",
+			"end":   "1511197250000"
+		},
+		{
+			"id": "297",
+			"title": "Pay Phone Bills",
+			"class": "event-warning",
+			"start": "1511366450000",
+			"end":   "1511368250000"
+		}
+	]
+}
